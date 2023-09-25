8623493e2d61075edf7c92d2fb11f615660d3220
Antonio Zea Jr <zeajantonio@gmail.com>
2023-09-24 22:56:48 -0400

    feat(svelte_app): :sparkles: add functional frontend and model
    This will serve as the basis of client side processing version of the application

--- svelte_app/cap_opp/src/lib/index.ts
+++ svelte_app/cap_opp/src/lib/index.ts
@@ -1,1 +1,77 @@
-// place files you want to import through the `$lib` alias in this folder.
+// import * as tf from '@tensorflow/tfjs';
+
+// // tf.ke
+
+// // export function ml_lib() {
+
+// // }
+
+// const tf = require('@tensorflow/tfjs-node');
+// const fs = require('fs');
+// const path = require('path');
+// const { promisify } = require('util');
+// const readdir = promisify(fs.readdir);
+// const sharp = require('sharp');
+
+// const imageDir = 'dataset/train'; // Adjust the directory paths
+// const validationDir = 'dataset/validate';
+
+// // Data preprocessing
+// const preprocessImage = async (filePath:any, targetSize:any) => {
+// 	const imageBuffer = await sharp(filePath).resize(targetSize[0], targetSize[1]).toBuffer();
+// 	return tf.node.decodeImage(imageBuffer, 3);
+// };
+
+// const readImagesFromDirectory = async (directoryPath, targetSize) => {
+// 	const imagePaths = await readdir(directoryPath);
+// 	const images = [];
+
+// 	for (const imagePath of imagePaths) {
+// 		const fullPath = path.join(directoryPath, imagePath);
+// 		const image = await preprocessImage(fullPath, targetSize);
+// 		images.push(image);
+// 	}
+
+// 	return tf.stack(images);
+// };
+
+// (async () => {
+// 	const targetSize = [224, 224];
+
+// 	const trainImages = await readImagesFromDirectory(imageDir, targetSize);
+// 	const validationImages = await readImagesFromDirectory(validationDir, targetSize);
+
+// 	const trainLabels = tf.tensor([...Array(trainImages.shape[0]).keys()]); // Modify for your binary labels
+// 	const validationLabels = tf.tensor([...Array(validationImages.shape[0]).keys()]); // Modify for your binary labels
+
+// 	// Model creation
+// 	const baseModel = await tf.loadLayersModel(
+// 		'https://tfhub.dev/tensorflow/resnet_50/classification/1'
+// 	);
+// 	const x = baseModel.predict(trainImages);
+// 	const output = tf.layers.dense({ units: 1, activation: 'sigmoid' }).apply(x);
+// 	const model = tf.model({ inputs: baseModel.input, outputs: output });
+
+// 	// Model compilation
+// 	model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
+
+// 	// Model training
+// 	await model.fit(trainImages, trainLabels, {
+// 		epochs: 10,
+// 		validationData: [validationImages, validationLabels]
+// 	});
+
+// 	const timestamp = new Date().toISOString().replace(/:/g, '-');
+// 	await model.save(`file://${path.join(__dirname, `models/model-${timestamp}`)}`);
+
+// 	// Predict new images
+// 	const newImage = await preprocessImage('00_001.png', targetSize);
+// 	const newImages = tf.stack([newImage]);
+// 	const prediction = await model.predict(newImages).data();
+
+// 	if (prediction[0] >= 0.5) {
+// 		console.log('Image belongs to the group.');
+// 	} else {
+// 		console.log('Image does not belong to the group.');
+// 	}
+// })();
--- svelte_app/cap_opp/src/routes/ImageGrid.svelte
+++ svelte_app/cap_opp/src/routes/ImageGrid.svelte
@@ -1,31 +1,53 @@
 <script lang="ts">
+	import { v4 as uuidv4 } from 'uuid';
 	export let title: string;
 	export let fileListParam: FileList | null = null;
+	export let handleFileListSubmit: (
+		fileList: {
+			file: File;
+			uuid: string;
+		}[]
+	) => void;
 	let imageGrid: HTMLDivElement;
 	let fileList: FileList | null;
 
 	if (fileListParam) {
-		populateImageGrid(fileListParam);
+		populateImageGrid(
+			Array.from(fileListParam).map((file) => {
+				return { file: file, uuid: uuidv4() };
+			})
+		);
 	} else {
 		fileList = null;
 	}
 
-	function populateImageGrid(fileList: FileList) {
-		const images = Array.from(fileList).map((file) => {
+	function populateImageGrid(
+		fileList: {
+			file: File;
+			uuid: any;
+		}[]
+	) {
+		const images = fileList.map(({ file, uuid }) => {
 			const reader = new FileReader();
 			reader.readAsDataURL(file);
-			return new Promise((resolve) => {
+			return new Promise<{
+				image: string;
+				uuid: string;
+			}>((resolve) => {
 				reader.onloadend = () => {
-					resolve(reader.result);
+					resolve({ image: reader.result as string, uuid: uuid });
 				};
 			});
 		});
 
 		Promise.all(images).then((images) => {
 			imageGrid.innerHTML = '';
-			images.forEach((image) => {
+			images.forEach(({ image, uuid }) => {
 				const img = document.createElement('img');
 				img.src = image as string;
+				img.id = uuid;
+				img.width = 224;
+				img.height = 224;
 				imageGrid.appendChild(img);
 			});
 		});
@@ -35,8 +57,13 @@
 		const inputElement = event.target as HTMLInputElement;
 		if (inputElement.files) {
 			fileList = inputElement.files;
-			populateImageGrid(fileList);
+
+			const testList = Array.from(fileList).map((file) => {
+				return { file: file, uuid: uuidv4() };
+			});
+			populateImageGrid(testList);
 			console.log(fileList);
+			handleFileListSubmit(testList);
 		}
 	}
 </script>
