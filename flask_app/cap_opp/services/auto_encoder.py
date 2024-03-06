42c3f6f30b813d7ebb0f7a5bd204c62e6d8ed94e
Antonio Zea Jr <zeajantonio@gmail.com>
2024-03-05 22:19:20 -0500

    Refactor image data augmentation and training process

--- flask_app/cap_opp/services/auto_encoder.py
+++ flask_app/cap_opp/services/auto_encoder.py
@@ -19,6 +19,7 @@ UpSampling2D = tf.keras.layers.UpSampling2D
 # TARGET_SIZE = (1920, 1080)
 TARGET_SIZE = (224, 224)
 BATCH_SIZE = 32
+DESIRED_DATASET_SIZE = 100
 
 
 class AutoEncoder(MlABC):
@@ -31,37 +32,54 @@ class AutoEncoder(MlABC):
     def process_training_images(self, img_path):
         self.logger.info("Processing Training Images", img_path=img_path)
 
-        train_ds: tf.data.Dataset = tf.keras.preprocessing.image_dataset_from_directory(
-            img_path,
-            labels=None,
-            # labels="inferred",
-            # label_mode="int",
-            # validation_split=0.2,
-            # subset="training",
-            seed=123,
-            image_size=TARGET_SIZE,
-            batch_size=BATCH_SIZE,
+        img_path = img_path.parent
+
+        datagen = ImageDataGenerator(
+            rescale=1.0 / 255,
+            rotation_range=20,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode="nearest",
         )
 
-        data_augmentation = tf.keras.Sequential(
-            [
-                tf.keras.layers.Rescaling(1.0 / 255),
-                tf.keras.layers.RandomFlip("horizontal_and_vertical"),
-                tf.keras.layers.RandomRotation(0.2),
-                tf.keras.layers.RandomZoom(0.2),
-                tf.keras.layers.RandomTranslation(height_factor=0.2, width_factor=0.2),
-                tf.keras.layers.RandomContrast(factor=0.2),
-            ]
+        augmented_ds = datagen.flow_from_directory(
+            img_path,
+            target_size=TARGET_SIZE,
+            batch_size=BATCH_SIZE,
+            class_mode="input",
+            shuffle=True,
         )
+
+        # train_ds: tf.data.Dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        #     img_path,
+        #     labels=None,
+        #     # labels="inferred",
+        #     # label_mode="int",
+        #     # validation_split=0.2,
+        #     # subset="training",
+        #     seed=123,
+        #     image_size=TARGET_SIZE,
+        #     batch_size=BATCH_SIZE,
+        # )
+
+        # data_augmentation = tf.keras.Sequential(
+        #     [
+        #         tf.keras.layers.Rescaling(1.0 / 255),
+        #         tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+        #         tf.keras.layers.RandomRotation(0.2),
+        #         tf.keras.layers.RandomZoom(0.2),
+        #         tf.keras.layers.RandomTranslation(height_factor=0.2, width_factor=0.2),
+        #         tf.keras.layers.RandomContrast(factor=0.2),
+        #     ]
+        # )
 
         # augmented_ds = train_ds.map(
-        #     lambda x, y: (data_augmentation(x, training=True), y),
+        #     lambda image: (data_augmentation(image, training=True), image),
         #     num_parallel_calls=tf.data.AUTOTUNE,
-        # ).repeat(desired_dataset_size // BATCH_SIZE)
-        augmented_ds = train_ds.map(
-            lambda image: (data_augmentation(image, training=True), image),
-            num_parallel_calls=tf.data.AUTOTUNE,
-        ).repeat()
+        # ).repeat(DESIRED_DATASET_SIZE // BATCH_SIZE)
 
         input_img = Input(shape=(224, 224, 3))
         x = Conv2D(32, (3, 3), activation="relu", padding="same")(input_img)
@@ -78,11 +96,25 @@ class AutoEncoder(MlABC):
         autoencoder = Model(input_img, decoded)
         autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
 
-        desired_dataset_size = 1000
+        desired_dataset_size = 100
         steps_per_epoch = desired_dataset_size // BATCH_SIZE
-        epochs = 1
+        epochs = 10
 
-        autoencoder.fit(augmented_ds, epochs=epochs, steps_per_epoch=steps_per_epoch)
+        # autoencoder.fit(
+        #     augmented_ds,
+        #     epochs=epochs,
+        #     # steps_per_epoch=steps_per_epoch,
+        # )
+
+        epochs = 100
+        steps_per_epoch = 1
+
+        for _ in range(epochs):
+            for _ in range(steps_per_epoch):
+                images, _ = next(augmented_ds)
+                self.logger.info(
+                    "training loss", loss=autoencoder.train_on_batch(images, images)
+                )
 
         self.autoencoder = autoencoder
 
@@ -98,6 +130,18 @@ class AutoEncoder(MlABC):
             )
         )
 
+        file_paths = [Path(path).name for path in collection_ds.file_paths]
+
+        data_augmentation = tf.keras.Sequential(
+            [
+                tf.keras.layers.Rescaling(1.0 / 255),
+            ]
+        )
+
+        collection_ds = collection_ds.map(
+            data_augmentation, num_parallel_calls=tf.data.AUTOTUNE
+        )
+
         self.logger.info("Processing Collection Images", img_path=img_path)
         self.logger.info("Collection DS", collection_ds=collection_ds)
 
@@ -106,14 +150,13 @@ class AutoEncoder(MlABC):
         reconstructed_images = []
 
         for batch in collection_ds:
-            self.logger.info("Batch", batch=batch)
+            # self.logger.info("Batch", batch=batch)
             # Get the reconstructed images
             reconstructed = self.autoencoder.predict(batch)
             # Calculate MAE for each image in the batch
             batch_errors = tf.reduce_mean(tf.abs(batch - reconstructed), axis=(1, 2, 3))
 
-
-            self.logger.info("Batch Errors", batch_errors=batch_errors)
+            # self.logger.info("Batch Errors", batch_errors=batch_errors)
 
             reconstruction_errors.extend(batch_errors)
             original_images.extend(batch.numpy())
@@ -123,13 +166,11 @@ class AutoEncoder(MlABC):
         originals = np.array(original_images)
         reconstructs = np.array(reconstructed_images)
 
-        file_paths = [Path(path).name for path in collection_ds.file_paths]
-
-        self.logger.info(
-            "Paths and Scores",
-            file_paths=file_paths,
-            errors=errors.tolist(),
-        )
+        # self.logger.info(
+        #     "Paths and Scores",
+        #     file_paths=file_paths,
+        #     errors=errors.tolist(),
+        # )
 
         # self.logger.info(
         #     "Paths and Scores",
