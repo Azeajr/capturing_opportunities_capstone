7d5f92bb748d094a4b607744eb0542e81c25a63b
Antonio Zea Jr <zeajantonio@gmail.com>
2024-03-08 13:50:27 -0500

    Add new files and configurations for Next.js project

---
+++ react_frontend/react-frontend/app/page.tsx
@@ -0,0 +1,17 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <Button variant="contained" color="primary">HELLOW WORLD</Button>
+      </div>
+    </main>
+  );
+}
