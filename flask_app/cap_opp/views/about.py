fb7725d218ba7c83ff3ad3ce9ed292954a13fbcc
Antonio Zea Jr <zeajantonio@gmail.com>
2024-04-03 20:32:29 -0400

    chore: :fire: Delete unnecessary flask and svelte app

--- flask_app/cap_opp/views/about.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-about_bp = Blueprint("about", __name__, template_folder="templates")
-
-
-@about_bp.route("/about")
-def index():
-    return render_template("about.jinja2")
