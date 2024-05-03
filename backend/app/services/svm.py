f6533217292fb509c5e5739771d42cb691a5cea1
Antonio Zea Jr <zeajantonio@gmail.com>
2024-05-02 22:38:25 -0400

    add charts for model performance comparison

--- backend/app/services/svm.py
+++ backend/app/services/svm.py
@@ -2,13 +2,13 @@ import math
 import pickle
 from pathlib import Path
 
+import keras
 import numpy as np
 import structlog
 import tensorflow as tf
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import OneClassSVM
-import keras
 
 from app.config import get_config
 from app.services.base import MlABC
